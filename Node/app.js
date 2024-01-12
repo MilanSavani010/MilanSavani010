ed7d99f3e1a08d68b88287e02bfc7ec1c9cc4300
milansavani010 <testypills@gmail.com>
2024-01-12 02:44:29 +0100

    commit

--- Node/app.js
+++ Node/app.js
@@ -1,13 +1,39 @@
-const http = require('http');
-const {greet} = require('./modules/utils')
+const express = require('express');
+const app = express();
 
-const server = http.createServer((req,res)=>{
-    res.writeHead(200,{'Content-Type':'text/plain'});
-    res.end('Hello, Node.js Server!');
-    greet();
+
+app.set('view engine','ejs');
+app.set('views','./views');
+
+/*
+Middleware functions are functions that have access to 
+the request object (req), the response object (res), 
+and the next middleware function in the application’s 
+request-response cycle.
+*/
+
+//Middleware functions
+const logMiddleware = (req, res, next)=>{
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+    next(); // Call the next middleware in the stack
+
+};
+
+app.use(logMiddleware);
+
+// Define a route for the root path
+app.get('/',(req,res)=>{
+    res.send('Hello, Express!');
 });
 
+app.get('/about',(req,res)=>{
+    res.send('About Page');
+});
+
+
+
+// Start the server
 const PORT = 3000;
-server.listen(PORT,()=>{
+app.listen(PORT,()=>{
     console.log(`Server is running on http://localhost:${PORT}`);
 });
